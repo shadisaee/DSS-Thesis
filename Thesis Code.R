3b60cb751ff30bb16a1b0588997ad68f73b40d25
Shadi Saee <49729649+shadisaee@users.noreply.github.com>
2019-11-02 22:14:45 +0100

    Add files via upload

--- Thesis Code.R
+++ Thesis Code.R
@@ -1,75 +1,109 @@
-#four_yr_fu_ids <- filter(total, Years_bl >= 4)$RID #3284
-#fu_avail <- total[which(total$RID %in% four_yr_fu_ids),]
-#BA_fu_ids <- filter(fu_avail, PLATFORM_ID == "BILEACID")$RID
-#final <- fu_avail[which(fu_avail$RID %in% BA_fu_ids),]
-
 #install.packages("Hmisc")
 #install.packages("ADNIMERGE_0.0.1.tar.gz", repos = NULL, type = "source")
 #install.packages(corrplot)
 
 #library(Hmisc) 
-library(ADNIMERGE) 
+#library(ADNIMERGE) 
 library(ggplot2) 
 library(dplyr)
 library(corrplot)
 
 # Load Data 
 adnimerge <- read.delim("ADNIMERGE.csv", sep = ',', stringsAsFactors = FALSE)
+vitals <- read.delim("VITALS.csv", sep = ',', stringsAsFactors = FALSE)
+postproc <- read.delim("Biospecimen_Results/ADMC_BA_POSTPROC_06_28_18.csv", 
+                       sep = ',', stringsAsFactors = FALSE)
+
 adni_dict <- read.delim("ADNIMERGE_DICT.csv", sep =',')
+postproc_dict <- read.delim("Biospecimen_Results/ADMC_BA_POSTPROC_06_28_18_DICT.csv", 
+                            sep = ',', stringsAsFactors = FALSE)
 
-postproc <- read.delim("Biospecimen_Results/ADMC_BA_POSTPROC_06_28_18.csv", sep = ',', stringsAsFactors = FALSE)
-postproc_dict <- read.delim("Biospecimen_Results/ADMC_BA_POSTPROC_06_28_18_DICT.csv", sep = ',', stringsAsFactors = FALSE)
-
-
-# Initial number of participants in ADNIMERGE & Postproc:
+# Initial number of participants in datasets
 count(distinct(adnimerge, RID)) #2231
 count(distinct(postproc, RID)) #1671
+count(distinct(vitals, RID)) #3282
 
-group_by(adnimerge, RID) %>% summarise(n_distinct(RID))
+#z <- adnimerge[which(adnimerge$RID %in% postproc$RID),]
+#count(distinct(z,RID))
+#group_by(z, DX_bl) %>% summarise(n_distinct(RID))
 
-z <- adnimerge[which(adnimerge$RID %in% postproc$RID),]
-count(distinct(z,RID))
-group_by(z, DX_bl) %>% summarise(n_distinct(RID))
-
-
-# Discard irrelevant variables & merge LMCI and EMCI diagnosis to MCI 
-postproc2 <- postproc[ , -c(5:9,34:57)] 
-adnimerge2 <- adnimerge[ , c(1,3,8:11,15,19:21,60,103:105,109:112)] %>%
+# Discard irrelevant variables 
+vitals2 <- vitals %>% select(RID, VISCODE, VSWEIGHT, VSWTUNIT, VSHEIGHT, VSHTUNIT)
+postproc2 <- postproc[ , -c(1,5:9,34:57)] 
+adnimerge2 <- adnimerge[ , c(1,3,8:11,15,19:21,60,103:105,109:110,112)] %>%
   mutate(DX_bl = ifelse(DX_bl == "LMCI"|DX_bl == "EMCI", 'MCI', DX_bl)) %>%
-  arrange(RID,VISCODE)
+  rename("GENDER" = PTGENDER, "EDUCATION" = PTEDUCAT) %>% 
+  mutate(GENDER = as.factor(GENDER)) %>%
+  arrange(RID, VISCODE)
 
-#count(distinct(adnimerge2, RID))
+# Process info for BMI meausure 
+bmi <- vitals2 %>% 
+  select(RID, VISCODE,VSHEIGHT, VSHTUNIT, VSWEIGHT, VSWTUNIT) %>%
+  mutate(VSHEIGHT = ifelse(VSHTUNIT == 1, VSHEIGHT * 2.54, VSHEIGHT)) %>%
+  mutate(VSWEIGHT = ifelse(VSWTUNIT == 1, VSWEIGHT / 2.205, VSWEIGHT)) %>%
+  filter(VSHEIGHT != -4 & !is.na(VSHEIGHT)) %>%
+  filter(VISCODE %in% c("bl", "sc", "v01")) %>%
+  arrange(RID)
 
-# Remove patients with SMC and include only those with data avail for three years
-#table(postproc2$SUBJECT_FLAG)
-group_by(adnimerge2, DX_bl) %>% summarise(n_distinct(RID))    #292 
+
+#Check if there is just one time of measurement per participant
+counts <- group_by(bmi, RID) %>% count()
+counts[counts$n > 1,] 
+
+# Check if heights are correct
+too_tall <- bmi[bmi$VSHEIGHT > 200,] 
+too_tall
+too_tall_ids <- too_tall[3:5,1]
+
+mutate_cond <- function(.data, condition, ..., envir = parent.frame()) {
+  condition <- eval(substitute(condition), .data, envir)
+  .data[condition, ] <- .data[condition, ] %>% mutate(...)
+  .data
+}
+# Source: https://stackoverflow.com/questions/34096162/dplyr-mutate-replace-several-columns-on-a-subset-of-rows
+  
+bmi <- bmi %>% mutate_cond(RID %in% too_tall_ids, VSHEIGHT = VSHEIGHT/2.54) 
+bmi[bmi$RID %in% too_tall_ids,]
+
+bmi <- bmi %>% 
+  mutate(VSHEIGHT = VSHEIGHT/100) %>%
+  mutate(BMI = VSWEIGHT/(VSHEIGHT*VSHEIGHT)) %>% 
+  mutate(VISCODE = "bl") %>%
+  select(-c(VSHTUNIT,VSWTUNIT))
+
+
+# Remove patients with SMC and include only those with data available for three years
+group_by(adnimerge2, DX_bl) %>% summarise(n_distinct(RID))   
   
 adnimerge2 <- adnimerge2 %>% 
   filter(DX_bl != 'SMC')  %>% 
   filter(Years_bl <= 3.2) %>% 
   group_by(RID) %>% 
   mutate(max_data = (max(M))) %>%
-  #ifelse(DX_bl != 'AD', filter(max_data == 36), )
   filter(max_data == 36) %>%
+  select(-c(max_data, Years_bl)) %>%
   ungroup(RID)
 
 #postproc2 <- filter(postproc2, SUBJECT_FLAG != 1)  #107
 #postproc_dict %>% filter(FLDNAME == 'SUBJECT_FLAG') %>% select(TEXT) 
 #postproc_dict %>% filter(FLDNAME == "TLCA_CDCA_LOGTRANSFORMFLAG") %>% select(TEXT) 
 
-# Merge data & rename variables
-total <- left_join(adnimerge2,postproc2, by = c("RID", "VISCODE")) %>% 
-  rename("GENDER" = PTGENDER, "EDUCATION" = PTEDUCAT) %>% arrange(RID, VISCODE)
-
-total$GENDER <-  as.factor(total$GENDER)
-
-# Total number of participants in ADNIMERGE & BA datasets
+# Merge data 
+total <- adnimerge2 %>%
+  left_join(bmi, c("RID", "VISCODE")) %>%
+  left_join(postproc2, by = c("RID", "VISCODE")) %>% 
+  arrange(RID, VISCODE)
+  
 count(distinct(total,RID)) #765
-count(distinct(postproc2,RID)) #1671
-
 
 # Only participants with BA info available 
 final <- total[which(total$RID %in% postproc2$RID),]
+
+vitalna <- total %>% 
+  filter(VISCODE == "bl") %>% 
+  sum(is.na(bmi)) #%>%
+  #count(is.na(VSHEIGHT)) 
+
 
 #`%not in%` <- function (x, table) is.na(match(x, table, nomatch=NA_integer_))
 
@@ -145,9 +179,9 @@ correlation_plot <- corrplot(correlations, method = "circle",
 correlation_plot
 
 
-MCI_patients[MCI_patients$VISCODE== 'm36',]$DX == "Dementia"
+#MCI_patients[MCI_patients$VISCODE== 'm36',]$DX == "Dementia"
 
-MCI_patients[VISCODE ==  'bl']$Final_DX <- 'Dementia'
+#MCI_patients[VISCODE ==  'bl']$Final_DX <- 'Dementia'
 
 
 
